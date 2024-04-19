a9fd675eec67ee7c3d11262a42db10685f7feb22
Gabriel Sussman <agabriel05@gmail.com>
2024-04-19 14:26:06 -0600

    Commit #2 Lab Completado

--- src/lab1p1_alejandrosussman/Lab1P1_AlejandroSussman.java
+++ src/lab1p1_alejandrosussman/Lab1P1_AlejandroSussman.java
@@ -4,6 +4,8 @@
  */
 package lab1p1_alejandrosussman;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Gabriel
@@ -15,6 +17,20 @@ public class Lab1P1_AlejandroSussman {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        System.out.println("BINEVENIDO A LA CALCULADORA DE PROMEDIOS");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String name = sc.nextLine();
+        System.out.println("Ingrese la nota de la primera evaluacion: ");
+        int nota1 = sc.nextInt();
+        System.out.println("Ingrese la nota de la segunda evaluacion: ");
+        int nota2 = sc.nextInt();
+        System.out.println("Ingrese la nota de la tercera evaluacion: ");
+        int nota3 = sc.nextInt();
+        double promedio;
+        promedio = (nota1 + nota2 + nota3) / 3;
+        System.out.println("La nota promedio del alumno " + name + "es de: " + promedio);
     }
-    
+
 }
